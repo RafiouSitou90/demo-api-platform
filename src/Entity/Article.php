e17c01a0573646248f0c3795ef2c7b1c88292e73
Rafiou Sitou <rafiousitou90@gmail.com>
2021-01-08 15:26:56 -0300

    Add article.

---
+++ src/Entity/Article.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Entity\Traits\ResourceIdTrait;
+use App\Entity\Traits\TimestampsTrait;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    use ResourceIdTrait;
+    use TimestampsTrait;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private string $content;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
